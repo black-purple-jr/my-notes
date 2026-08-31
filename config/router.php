86e0b84d44a5d56c40a1f76cb328e239d642a603
Abdellah DAKIR ALLAH <dal.dakirallah@gmail.com>
2026-08-31 01:15:58 +0100

    breaking code

--- config/router.php
+++ config/router.php
@@ -1,2 +1,13 @@
 <?php
-define("BASE_URL", "http://localhost/my-notes");
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . "/..");
+$dotenv->safeLoad();
+
+define(
+  "BASE_URL",
+  rtrim($_ENV["BASE_URL"] ?? "http://localhost:8080", "/")
+);
